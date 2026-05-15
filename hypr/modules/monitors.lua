a020071702a8606754939e68c941460c594c99b0
Frank1o3 <jahdy1o3@gmail.com>
2026-05-15 19:42:16 -0400

    general monitor config + dependencie list

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -2,16 +2,9 @@
 
 return function()
 	hl.monitor({
-		output = "DP-1",
-		mode = "1920x1200@59.95Hz",
-		position = "-1920x-30",
-		scale = 1.0,
-		cm = "srgb",
-	})
-	hl.monitor({
-		output = "HDMI-A-1",
-		mode = "1920x1080@60.00Hz",
-		position = "10x-30",
+		output = "",
+		mode = "preferred",
+		position = "auto",
 		scale = 1.0,
 		cm = "srgb",
 	})
