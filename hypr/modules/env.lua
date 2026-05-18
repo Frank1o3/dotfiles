d12ccac960762c781d2bcae49e01754ac882954d
Frank1o3 <jahdy1o3@gmail.com>
2026-05-18 06:51:29 -0400

    eee

--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -1,27 +1,26 @@
 -- ~/.config/hypr/modules/env.lua
 
 return function()
-    -- QT Environment Variables\
+    -- QT
     hl.env("QT_STYLE_OVERRIDE", "gtk3")
     hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 
-    -- GTK Environment Variables
-    hl.env("GTK_THEME", "Adwaita:Dark")
-    hl.env("GDK_THEME", "Adwaita:Dark")
+    -- GTK
+    hl.env("GTK_THEME", "Adwaita-dark")
     hl.env("GTK_ICON_THEME", "Papirus-Dark")
     hl.env("GTK_CURSOR_THEME", "catppuccin-mocha-dark-cursors")
     hl.env("GTK_APPLICATION_PREFER_DARK_THEME", "1")
+
+    -- Scaling
     hl.env("GDK_SCALE", "1")
 
-    -- Cursor Size
+    -- Cursor
     hl.env("XCURSOR_SIZE", "20")
     hl.env("HYPRCURSOR_SIZE", "20")
     hl.env("XCURSOR_THEME", "catppuccin-mocha-dark-cursors")
     hl.env("HYPRCURSOR_THEME", "catppuccin-mocha-dark-cursors")
 
-
-    -- XDG Environment Variables
+    -- Session
     hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
     hl.env("XDG_SESSION_TYPE", "wayland")
-    hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 end
