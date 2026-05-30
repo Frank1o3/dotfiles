afea55b507e84d4307e7d4774cfe3928fa97c9b9
Frank1o3 <jahdy1o3@gmail.com>
2026-05-30 09:22:22 -0400

    release: Minor changes to the theming

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -5,7 +5,7 @@ return function(WALLPAPER_DIR)
 	hl.on("hyprland.start", function()
 		-- Update dbus/systemd activation environment so launched apps see the new vars
 		hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-		hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark")
+		hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark")
 		hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark")
 		hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme catppuccin-mocha-dark-cursors")
 
