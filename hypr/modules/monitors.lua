6df979daa63f8eb3865896b548004ae9d967125e
Frank1o3 <jahdy1o3@gmail.com>
2026-06-13 12:00:36 -0400

    Auto-commit: 2026-06-13 12:00:36

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -1,5 +1,7 @@
 -- ~/.config/hypr/monitors.lua
 
+local primary_monitor = "DP-3"
+
 -- 1. Secondary monitor positioned to the right
 hl.monitor({
 	output = "HDMI-A-1",
@@ -11,7 +13,7 @@ hl.monitor({
 
 -- 2. Primary monitor positioned at 0x0
 hl.monitor({
-	output = "DP-4",
+	output = primary_monitor,
 	mode = "1920x1200@60Hz",
 	position = "0x0",
 	scale = 1.0,
@@ -22,7 +24,7 @@ hl.monitor({
 -- 3. Set Workspace 1 as the default and make it persistent on your primary monitor
 hl.workspace_rule({
 	workspace = "1",
-	monitor = "DP-4",
+	monitor = primary_monitor,
 	default = true,
 	persistent = true,
 })
